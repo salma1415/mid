2a3dbb10947ab67056bb8b70aa9305074db1208e
salma1415 <32789490+salma1415@users.noreply.github.com>
2020-10-26 14:29:08 +0300

    Add files via upload

---
+++ javaS.js
@@ -0,0 +1,48 @@
+// document.querySelector(".scorePlayer1").textContent;
+// document.querySelectorAll('img')[1].setAttribute;
+// function choose() {
+//   var game = document.forms[0];
+//   var txt = "";
+//   var i;
+//   for (i = 0; i < game.length; i++) {
+//     if (game[i].checked) {
+//       txt = txt + game[i].value + " ";
+//     }
+//   }
+//   //document.getElementById("order").value = "You ordered a coffee with: " + txt;
+//   document.querySelector(".display").value= "You ordered a game with: " + txt;
+// }
+
+//
+// var choose1 = document.querySelectorAll('gameForm')[3].value;
+// var choose2 = document.querySelectorAll('gameForm')[5].value;
+// function Choose(){
+// if (choose1){
+// randomColor();
+// }
+// else{
+//     disappearingNumbers();
+// }
+//
+// }
+//
+// function randomColor(){
+//
+// }
+// function disappearingNumbers(){
+//
+// }
+// var b1 = document.querySelector(".box1").style.display = "none";
+// var b2 = document.querySelector(".box2").style.display = "none";
+function disappear() {
+
+// document.querySelector(".box1").style.display = "none";
+// document.querySelector(".box2").style.display = "none";
+// document.querySelector(".box3").style.display = "none";
+// document.querySelector(".box4").style.display = "none";
+// document.querySelector(".box5").style.display = "none";
+// document.querySelector(".box6").style.display = "none";
+// document.querySelector(".box7").style.display = "none";
+// document.querySelector(".box8").style.display = "none";
+// document.querySelector(".box9").style.display = "none";
+}
